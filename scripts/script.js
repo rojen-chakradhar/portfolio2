2c69728bc378325fb0c37c206461039c55eafdda
Rojen Chakradhar <chakradhar@rojen.me>
2026-02-03 22:51:36 +0545

    prev idea discard and new started

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-function menu(action) {
-  let navLinks = document.getElementById('nav-links');
-  if (action == 'open') {
-    navLinks.style.display = "flex";
-  } else {
-    navLinks.style.display = "none";
-  }
-}
